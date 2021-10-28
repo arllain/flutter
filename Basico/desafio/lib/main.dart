ed3a734514f4e36398b80be8f00b7b429df7e3be
Arllain <arllain@gmail.com>
2021-10-28 20:45:42 -0300

    Conclusão do desafio

--- Basico/desafio/lib/main.dart
+++ Basico/desafio/lib/main.dart
@@ -5,30 +5,17 @@ void main() => runApp(AgcsApp());
 
 class AgcsApp extends StatelessWidget {
   @override
-  Widget build(BuildContext conxtext) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: "Desafio",
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
-    ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Desafio Flutter Básico"),
         ),
-        body: Column(
-          children: <Widget>[
-            Flexible(flex: 1,
-                child: ElevatedButton(
-                  child: const Text('Resetar todos os itens', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold )),
-                  onPressed: () {},
-                ),
-            ),
-            const Flexible(flex: 6,
-                child: AgcsWidget(),
-            )
-          ],
-
-        ),
+        body: const AgcsWidget(),
       ),
     );
   }
--- Basico/desafio/lib/widgets/mywidget.dart
+++ Basico/desafio/lib/widgets/mywidget.dart
@@ -3,8 +3,6 @@ import 'package:desafio/repository/repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
-
 class AgcsState extends State<AgcsWidget> {
 
   var _items = <Item>[];
@@ -12,7 +10,7 @@ class AgcsState extends State<AgcsWidget> {
   @override
   void initState() {
     super.initState();
-    
+
     _loadData();
   }
 
@@ -23,17 +21,53 @@ class AgcsState extends State<AgcsWidget> {
     });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: <Widget>[
+          _buildResetButton(),
+          _buildListView()
+        ]
+    );
+  }
+
+
+  Widget _buildResetButton() {
+    return Flexible(
+        flex: 1,
+        child: ElevatedButton(
+          child: const Text('Resetar todos os itens', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold )),
+          onPressed: () {
+            _loadData();
+          },
+        )
+    );
+  }
+
+  Widget _buildListView() {
+    return Flexible(
+      flex: 6,
+      child: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: _items.length,
+        itemBuilder: (BuildContext context, int position){
+          return _buildRow(position);
+        },
+      ),
+    );
+  }
+
   Widget _buildRow(int position) {
     return Card(
       elevation: 0.2,
       child: ListTile(
-        title: Text(_items[position].content, style: getTextStyle(_items[position].isChecked)),
-        leading: Text("${_items[position].id}", style: getTextStyle(_items[position].isChecked)),
-        trailing: Checkbox(
-          value: _items[position].isChecked,
-          onChanged: (value) => setState(() => _items[position].isChecked = value!)
-        ),
-        tileColor:  getColor(_items[position].isChecked)
+          title: Text(_items[position].content, style: getTextStyle(_items[position].isChecked)),
+          leading: Text("${_items[position].id}", style: getTextStyle(_items[position].isChecked)),
+          trailing: Checkbox(
+              value: _items[position].isChecked,
+              onChanged: (value) => setState(() => _items[position].isChecked = value!)
+          ),
+          tileColor:  getColor(_items[position].isChecked)
       ),
     );
   }
@@ -41,18 +75,6 @@ class AgcsState extends State<AgcsWidget> {
   Color getColor(bool isChecked) => ( isChecked ? Colors.black : Colors.white);
   TextStyle getTextStyle(bool isChecked) => (isChecked ? const TextStyle(fontSize: 15.0, color: Colors.white)
       : const TextStyle(fontSize: 15.0, color: Colors.black));
-
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemCount: _items.length,
-      itemBuilder: (BuildContext context, int position){
-        return _buildRow(position);
-      },
-    );
-  }
 }
 
 
