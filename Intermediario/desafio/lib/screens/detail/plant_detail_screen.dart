54f8151b91219df2941bcdcc8cb6e2907d712cc3
Arllain <arllain@gmail.com>
2021-11-14 18:24:44 -0300

    Na tela 2, deve adicionar um botão de back, e ao ser clicado deve retornar para a tela 1

--- Intermediario/desafio/lib/screens/detail/plant_detail_screen.dart
+++ Intermediario/desafio/lib/screens/detail/plant_detail_screen.dart
@@ -2,6 +2,8 @@ import 'package:challenge_ui_plant_app/models/plant.dart';
 import 'package:challenge_ui_plant_app/screens/detail/components/plant_detail_body.dart';
 import 'package:flutter/material.dart';
 
+import '../../constants.dart';
+
 class PlantDetailScreen extends StatelessWidget {
   const PlantDetailScreen({Key? key, required this.plant}) : super(key: key);
 
@@ -9,9 +11,18 @@ class PlantDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(),
       body: PlantDetailBody(
         plant: plant,
       ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      //title: const Text("Plant Detail"),
+      backgroundColor: kPrimaryColor,
+      elevation: 1,
     );
   }
 }
